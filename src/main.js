c31424a91a37f30aec7c4caad6e56c987fb3b198
Mangopapa1 <gustp26@gmail.com>
2023-02-23 16:41:03 +0900

    Feat: main-component 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { navComponent } from "./component/NavComponent.js";
+import { mainComponent } from "./component/MainComponent.js";
 import { footerComponent } from "./component/FooterComponent.js";
 
 document.querySelector('#app').innerHTML += `
     ${navComponent}
+    ${mainComponent}
     ${footerComponent}
 `
