38d8ff511bfcb8d385f1a7c31d6d2b5a0600b9f7
nara04040 <nara04040@naver.com>
2023-02-28 12:22:44 +0900

    Feat : JSON데이터를 MainComponent list가 데이터 개수만큼 추가 구현

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -8,24 +8,48 @@ class MainComponent {
   render() {
     const json = JSON.stringify(MOCK_DATA);
     const jsonDataList = JSON.parse(json)
-
-    for(let newArr of jsonDataList) {
-      return `
-      <a id="main-container">
-        <img class="logo-img" src="./src/img/content1.png" alt="컨텐츠 이미지">
-        <div>
-          <h4 class="txt-title">${newArr.title}</h4>
-          <span class="txt-content">
-          ${newArr.content}
-          </span>
-          <span class="txt-date">${newArr.date}</span>
-        </div>
-      </a>
-`;
+    let makeList = ``
+    
+    for(let i = 0; i < 14; i++) {
+        makeList += `
+        <a id="main-container">
+          <img class="logo-img" src="./src/img/content1.png" alt="컨텐츠 이미지">
+          <div>
+          ${this.totalComponent(jsonDataList[i].title, jsonDataList[i].content, jsonDataList[i].date)}          
+          </div>
+        </a>
+        `
     }
-
-      
+    return makeList
   };
+
+  totalComponent(title, content, date) {
+    return `
+        ${this.titleComponent(title)}
+        ${this.contentComponent(content)}
+        ${this.dateComponent(date)}
+    `
+  }
+
+  titleComponent(title) {
+    return `
+        <h4 class="txt-title">${title}</h4>
+    `
+  }
+
+  contentComponent(content) {
+    return `
+      <span class="txt-content">
+        ${content}
+      </span>      
+    `
+  }
+
+  dateComponent(date) {
+    return `
+      <span class="txt-date">${date}</span>    
+    `
+  }
 }
 
 export default MainComponent;
