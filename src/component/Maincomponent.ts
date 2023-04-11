8cfc65c592e82f9ae54f33b43166c92ab7ac5af9
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-04-11 13:37:36 +0900

    Issue 42 main component type script로 마이그래이션 (#43)
    * Feat: TypeScript에서 json import 기능 구현
    
    * Refactor: TypeScript로 마이그래이션 진행
    
    * Refactor: 접근제어자, 제네릭 적용
    
    * Fix : $container타입을 구체적으로 표기

---
+++ src/component/Maincomponent.ts
@@ -0,0 +1,82 @@
+import MOCK_DATA from '../../TECH_MOCK_DATA.json';
+
+interface Article {
+  title: string;
+  summary: string;
+  date: string;
+  id: number;
+}
+
+class MainComponent {
+  private $container: HTMLDivElement;
+
+  constructor($container: HTMLDivElement) {
+    this.$container = $container;
+  }
+
+  render(): string {
+    return `
+    <h3 class='main-subject'>개발</h3>
+    ${this.jsonRender()}
+    `;
+  }
+
+  private jsonRender(): string {
+    const jsonDataList = MOCK_DATA;
+    let result: string = '';
+
+    jsonDataList.reduce((acc: string, { title, summary, date, id }) => {
+      acc += `
+        <a id="main-container" href='/article/${id}'>
+          <img class="logo-img" src="./src/img/content1.png" alt="컨텐츠 이미지" data-link>
+          <div>
+            ${this.totalComponent(title, summary, date)}
+          </div>
+        </a>    
+        `;
+
+      result = acc;
+      return acc;
+    }, '');
+    return result;
+  }
+
+  private totalComponent(title: string, summary: string, date: string): string {
+    return `
+        ${this.titleComponent(title)}
+        ${this.contentComponent(summary)}
+        ${this.dateComponent(date)}
+    `;
+  }
+
+  private titleComponent(title: string): string {
+    return `
+        <h4 class="txt-title">${title}</h4>
+    `;
+  }
+
+  private contentComponent(summary: string): string {
+    return `
+      <span class="txt-content">
+        ${summary}
+      </span>      
+    `;
+  }
+
+  private Unix_timestamp(t: any): string {
+    let date = new Date(t * 1000);
+    let year = date.getFullYear();
+    let month = '0' + (date.getMonth() + 1);
+    let day = '0' + date.getDate();
+
+    return year + '-' + month.slice(-2) + '-' + day.slice(-2) + ' ';
+  }
+
+  private dateComponent(date: string): string {
+    return `
+      <span class="txt-date">${this.Unix_timestamp(date)}</span>
+    `;
+  }
+}
+
+export default MainComponent;
